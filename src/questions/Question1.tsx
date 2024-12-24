6d17620bc0c9adb9ccd393c50565218254855a12
Ajay Anto <120694977+AjayAntoIsDev@users.noreply.github.com>
2024-12-24 21:25:30 +0530

    more stuff

--- src/questions/Question1.tsx
+++ src/questions/Question1.tsx
@@ -1,15 +1,23 @@
+import { ArrowRight } from 'lucide-react';
+
 export const Question1 = () => {
   return (
-    <div className="flex flex-col justify-center items-center text-center h-full">
+    <div className="flex h-full flex-col items-center justify-center text-center">
       <h1 className="text-5xl font-bold">
         I’m interested in{' '}
         <input
           type="text"
-          placeholder="technology, art, helping the environment"
-          className="bg-transparent border-b-[6px] border-surface-tonal-a50 focus:outline-none text-white placeholder:text-surface-tonal-a50 blinking-underline !underline-offset-8 w-[57.7vw]"
+          placeholder="technology, art, fashion, etc."
+          className="blinking-underline w-[57.7vw] border-b-[6px] border-surface-tonal-a50 bg-transparent text-white !underline-offset-8 placeholder:text-surface-tonal-a50 focus:outline-none"
         />
       </h1>
-      <button className={"text-2xl bg-primary-a30 text-black p-2 rounded-lg px-5 font-semibold mt-6"}>Next</button>
+      <button
+        className={
+          'mt-8 flex items-center justify-center gap-2 rounded-lg bg-primary-a30 p-2 px-5 text-2xl font-semibold text-surface-a0'
+        }
+      >
+        Next <ArrowRight strokeWidth={'3px'} height={'26px'} width={'26px'} />
+      </button>
     </div>
   );
 };
